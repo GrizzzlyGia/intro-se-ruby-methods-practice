182fa55492e80d7f082dabb4f764415af4a57412
Gia Johnson <johnsongia18@gmail.com>
2017-06-20 19:15:54 +0000

    not finished with number five and number 6

---
+++ methods-practice.rb
@@ -0,0 +1,19 @@
+def timesOneThousand (number)
+    number*1000.0
+end
+puts timesOneThousand (8)
+
+def howOldWillBeIn2030(year)
+    15 + year
+end
+puts howOldWillBeIn2030(13)
+
+def makeExcited(word)
+    "#{word}".upcase + "!!!"
+end
+puts makeExcited("hello")
+
+def happyBackwards (word)
+    "#{word}".reverse + ":)"
+end
+puts happyBackwards("cool")
